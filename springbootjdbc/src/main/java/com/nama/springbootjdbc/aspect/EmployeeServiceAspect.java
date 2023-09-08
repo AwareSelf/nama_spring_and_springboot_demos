fe1a414cb11af264bc979345f68fe6c755ad7d4a
AwareSelf <namratamarathe81@gmail.com>
2023-09-08 12:42:44 +0530

    aop aspect file changed
    aop aspect file changed

--- springbootjdbc/src/main/java/com/nama/springbootjdbc/aspect/EmployeeServiceAspect.java
+++ springbootjdbc/src/main/java/com/nama/springbootjdbc/aspect/EmployeeServiceAspect.java
@@ -2,27 +2,34 @@ package com.nama.springbootjdbc.aspect;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import com.nama.springbootjdbc.beans.Emp;
+import com.nama.springbootjdbc.service.EmployeeService;
 
 @Aspect
 @Component
 public class EmployeeServiceAspect {
 
+	Logger log = LoggerFactory.getLogger(EmployeeService.class);
+	
 	@Before(value = "execution(* com.nama.springbootjdbc.service.EmployeeService.*(..)) "
 			+ "and args(empId,empName)")
 	public void beforeAdvice(JoinPoint joinPoint, int empId,String empName) {
-		System.out.println("Before method:" + joinPoint.getSignature());
+		log.info("aspect:Before method:" + joinPoint.getSignature());
 
-		System.out.println("Creating Employee with name - " + empName + " and id - " + empId);
+		log.info("aspect:Creating Employee with name - " + empName + " and id - " + empId);
 	}
 
-	@After(value = "execution(* com.nama.springbootjdbc.service.EmployeeService.*(..)) "
-			+ "and args(empId,empName)")
-	public void afterAdvice(JoinPoint joinPoint, int empId,String empName) {
-		System.out.println("After method:" + joinPoint.getSignature());
-
-		System.out.println("Successfully created Employee with name - " + empName + " and id - " + empId);
+		
+	@AfterReturning(pointcut="execution(* com.nama.springbootjdbc.service.EmployeeService.*(..))", returning="emp") 
+	public void logReturnValue(JoinPoint joinPoint,Emp emp)
+	{
+		log.info("aspect:emp return from service.EmployeeService"+emp);
 	}
 }
